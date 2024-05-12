b74fec1e0e943a4dfaac6cdfca8a944f335bfd36
John Kenny <johnkenny54@yahoo.com>
2024-05-12 06:41:30 -0700

    Updated TypeScript.

--- app/js/lib/coldef.js
+++ app/js/lib/coldef.js
@@ -1,42 +1,55 @@
 import { DOMUtils } from "./domutils.js";
+// eslint-disable-next-line no-unused-vars
+import { UI } from "./ui.js";
 
 class ColDef {
-
     #th;
     #fnValue;
     #className;
 
-    constructor( th, fnValue, className ) {
+    /**
+     * @param {string} th
+     * @param {function (any,UI) : Element|string} fnValue
+     * @param {string} [className]
+     */
+    constructor(th, fnValue, className) {
         this.#th = th;
         this.#fnValue = fnValue;
         this.#className = className;
     }
 
-    static addColElement( tr, content, className ) {
-        const td = DOMUtils.createElement( "td", className );
-        if ( content instanceof Node ) {
-            td.appendChild( content );
+    /**
+     * @param {Element} tr
+     * @param {Node|string} content
+     * @param {string} className
+     */
+    static addColElement(tr, content, className) {
+        const td = DOMUtils.createElement("td", className);
+        if (content instanceof Node) {
+            td.appendChild(content);
         } else {
-            td.appendChild( document.createTextNode( content ) );
+            td.appendChild(document.createTextNode(content));
         }
-        tr.appendChild( td );
+        tr.appendChild(td);
     }
 
-    static createTable( cols ) {
-        const table = DOMUtils.createElement( "table" );
+    /**
+     * @param {ColDef[]} cols
+     */
+    static createTable(cols) {
+        const table = DOMUtils.createElement("table");
 
-        const thead = DOMUtils.createElement( "thead" );
-        table.appendChild( thead );
-        const tr = DOMUtils.createElement( "tr" );
-        thead.appendChild( tr );
-        for ( const col of cols ) {
-            const th = DOMUtils.createElement( "th", { class: col.getClass() } );
-            tr.appendChild( th );
-            th.appendChild( document.createTextNode( col.getHeaderLabel() ) );
+        const thead = DOMUtils.createElement("thead");
+        table.appendChild(thead);
+        const tr = DOMUtils.createElement("tr");
+        thead.appendChild(tr);
+        for (const col of cols) {
+            const th = DOMUtils.createElement("th", col.getClass());
+            tr.appendChild(th);
+            th.appendChild(document.createTextNode(col.getHeaderLabel()));
         }
 
         return table;
-
     }
 
     getClass() {
@@ -47,10 +60,13 @@ class ColDef {
         return this.#th;
     }
 
-    getValue( entry, ui ) {
-        return this.#fnValue( entry, ui );
+    /**
+     * @param {any} entry
+     * @param {UI} ui
+     */
+    getValue(entry, ui) {
+        return this.#fnValue(entry, ui);
     }
-
 }
 
 export { ColDef };
