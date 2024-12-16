486f6efa53168d8cb6dd86ed58d5427d5df1b361
John Kenny <johnkenny54@yahoo.com>
2024-12-16 14:17:08 -0800

    feat: add borders to clusters

--- app/js/tools/clusterer.js
+++ app/js/tools/clusterer.js
@@ -1,22 +1,73 @@
 export class Clusterer {
     #clustersDbscan;
+    #convex;
     #turfMeta;
+    #turfHelpers;
 
     /**
      * @param {import("@turf/clusters-dbscan").clustersDbscan} clustersDbscan
      * @param {import("@turf/meta")} turfMeta
+     * @param {import("@turf/helpers")} turfHelpers
+     * @param {import("@turf/clone")} clone
+     * @param {import("@turf/convex").convex} convex
      */
-    constructor(clustersDbscan, turfMeta) {
+    constructor(clustersDbscan, turfMeta, turfHelpers, clone, convex) {
         this.#clustersDbscan = clustersDbscan;
         this.#turfMeta = turfMeta;
+        this.#turfHelpers = turfHelpers;
+        this.#convex = convex;
+    }
+
+    /**
+     * @param {import("geojson").FeatureCollection} geojson
+     * @param {{includeClusteredPoints?:boolean}} [options]
+     * @param {import("geojson").GeoJsonProperties} [properties={}]
+     * @returns {import("geojson").FeatureCollection}
+     */
+    addBorders(geojson, options = {}, properties = {}) {
+        /** @type {import("geojson").Feature[]} */
+        const newFeatures = [];
+
+        /** @type {Map<number,import("geojson").Feature[]>} */
+        const clusters = new Map();
+        this.#turfMeta.featureEach(geojson, (f) => {
+            if (!f.properties) {
+                return;
+            }
+            if (typeof f.properties.cluster === "number") {
+                let features = clusters.get(f.properties.cluster);
+                if (!features) {
+                    features = [];
+                    clusters.set(f.properties.cluster, features);
+                }
+                features.push(f);
+                if (options.includeClusteredPoints) {
+                    newFeatures.push(f);
+                }
+            } else {
+                newFeatures.push(f);
+            }
+        });
+
+        for (const features of clusters.values()) {
+            const fc = this.#turfHelpers.featureCollection(features);
+            const border = this.#convex(fc);
+            if (border) {
+                border.properties = { ...properties };
+                newFeatures.push(border);
+            }
+        }
+
+        return this.#turfHelpers.featureCollection(newFeatures);
     }
 
     /**
      * @param {GeoJSON.FeatureCollection<import("geojson").Point>} geojson
+     * @param {number} [maxDistance]
      * @returns {import("geojson").FeatureCollection}
      */
-    cluster(geojson) {
-        return this.#clustersDbscan(geojson, 1);
+    cluster(geojson, maxDistance = 1) {
+        return this.#clustersDbscan(geojson, maxDistance);
     }
 
     /**
--- scripts/int-cluster.js
+++ scripts/int-cluster.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 import clustersDbscan from "@turf/clusters-dbscan";
+import convex from "@turf/convex";
+import * as clone from "@turf/clone";
 import * as turfMeta from "@turf/meta";
+import * as turfHelpers from "@turf/helpers";
 import { Command } from "commander";
 import { readFileSync, writeFileSync } from "node:fs";
 import * as path from "node:path";
@@ -10,32 +13,38 @@ import { Clusterer } from "../app/js/tools/clusterer.js";
 
 class NodeClusterer extends Clusterer {
     constructor() {
-        super(clustersDbscan, turfMeta);
+        super(clustersDbscan, turfMeta, turfHelpers, clone, convex);
     }
 }
 
 const __dirname = cwd();
 
 /**
+ * @param {Command} program
  * @param {import("commander").OptionValues} options
  */
-async function run(options) {
-    const str = readFileSync(
-        path.join(__dirname, "tmp/calochortus_umbellatus.geojson"),
-        "utf8"
-    );
+async function run(program, options) {
+    if (!options.input) {
+        program.help();
+    }
+    const str = readFileSync(path.join(__dirname, options.input), "utf8");
     /** @type {GeoJSON.FeatureCollection<GeoJSON.Point>} */
     const json = JSON.parse(str);
     const clusterer = new NodeClusterer();
     const clustered = clusterer.cluster(json);
-    console.log(clusterer.getMaxCluster(clustered));
+    const bordered = clusterer.addBorders(
+        clustered,
+        {},
+        { fill: "red", "fill-opacity": 0.8 }
+    );
     writeFileSync(
         path.join(__dirname, "tmp/clustered.geojson"),
-        JSON.stringify(clustered)
+        JSON.stringify(bordered)
     );
 }
 
 const program = new Command();
-program.action((options) => run(options));
+program.option("-i, --input <path>", "The path to the input file.");
+program.action((options) => run(program, options));
 
 await program.parseAsync();
