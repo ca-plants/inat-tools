9bb2affed7f84af09e704ca94e72ee8f6ca46c9c
John Kenny <johnkenny54@yahoo.com>
2024-12-16 08:27:00 -0800

    chore: implement commander

--- build.js
+++ build.js
@@ -3,45 +3,52 @@ import { dirname } from "path";
 import Metalsmith from "metalsmith";
 import layouts from "@metalsmith/layouts";
 import inPlace from "@metalsmith/in-place";
-import commandLineArgs from "command-line-args";
+import { Command } from "commander";
 
-const OPTION_DEFS = [
-    { name: "path", type: String, defaultValue: "/" },
-    { name: "watch", type: Boolean },
-];
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const options = commandLineArgs( OPTION_DEFS );
+/**
+ * @param {import("commander").OptionValues} options
+ */
+async function build(options) {
+    const nunjuckOptions = {
+        transform: "nunjucks",
+        engineOptions: {
+            root: __dirname + "/layouts",
+            globals: {
+                homePath: options.path,
+            },
+        },
+    };
 
-const __dirname = dirname( fileURLToPath( import.meta.url ) );
+    const ms = Metalsmith(__dirname);
 
-const nunjuckOptions = {
-    transform: "nunjucks",
-    engineOptions: {
-        root: __dirname + "/layouts",
-        globals: {
-            homePath: options.path
-        }
+    ms.directory(__dirname).source("./app").destination("./public").clean(true);
+
+    if (options.watch) {
+        ms.watch(["./app", "./layouts"]);
     }
-};
 
-const ms = Metalsmith( __dirname );
-
-ms.directory( __dirname )
-    .source( "./app" )
-    .destination( "./public" )
-    .clean( true );
-
-if ( options.watch ) {
-    ms.watch( [ "./app", "./layouts" ] );
-}
-
-ms.use( inPlace( { transform: "marked" } ) )
-    .use( inPlace( nunjuckOptions ) )
-    .use( layouts( nunjuckOptions ) )
-    .build(
-        ( err ) => {
-            if ( err ) {
+    ms.use(inPlace({ transform: "marked" }))
+        .use(inPlace(nunjuckOptions))
+        .use(layouts(nunjuckOptions))
+        .build((err) => {
+            if (err) {
                 throw err;
             }
-        }
-    );            
+        });
+}
+
+const program = new Command();
+program.option(
+    "--path <path>",
+    "The path to the directory containing Metalsmith files.",
+    "/"
+);
+program.option(
+    "--no-watch",
+    "Generate the output files, but do not continue to watch."
+);
+program.action((options) => build(options));
+
+await program.parseAsync();
