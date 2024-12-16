5210b11592f013b78f8d90ed9fffb5a002d26fa7
johnkenny54 <45182853+johnkenny54@users.noreply.github.com>
2024-12-16 09:05:39 -0800

    chore: implement commander (#17)
    * chore: implement commander
    
    * chore: move build to scripts directory

---
+++ scripts/int-build.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+import Metalsmith from "metalsmith";
+import layouts from "@metalsmith/layouts";
+import inPlace from "@metalsmith/in-place";
+import { Command } from "commander";
+import { cwd } from "node:process";
+
+const __dirname = cwd();
+
+/**
+ * @param {import("commander").OptionValues} options
+ */
+async function build(options) {
+    const nunjuckOptions = {
+        transform: "nunjucks",
+        engineOptions: {
+            root: __dirname + "/layouts",
+            globals: {
+                homePath: options.homepath,
+            },
+        },
+    };
+
+    const ms = Metalsmith(__dirname);
+
+    ms.directory(__dirname).source("./app").destination("./public").clean(true);
+
+    if (options.watch) {
+        ms.watch(["./app", "./layouts"]);
+    }
+
+    ms.use(inPlace({ transform: "marked" }))
+        .use(inPlace(nunjuckOptions))
+        .use(layouts(nunjuckOptions))
+        .build((err) => {
+            if (err) {
+                throw err;
+            }
+        });
+}
+
+const program = new Command();
+program.option(
+    "--homepath <path>",
+    "The path to the home directory of the target site.",
+    "/"
+);
+program.option(
+    "--no-watch",
+    "Generate the output files, but do not continue to watch."
+);
+program.action((options) => build(options));
+
+await program.parseAsync();
