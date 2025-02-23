79acad276c8628148941f3b5890f26dc4db2a7ca
johnkenny54 <45182853+johnkenny54@users.noreply.github.com>
2025-02-23 08:00:16 -0800

    chore: add clustering features (#29)
    * feat: show convex outline of cluster
    
    * chore: calculate polygon area
    
    * feat: show points outside of final polygon
    
    * chore: show points that are not in a polygon
    
    * chore: convert to turf 7.2

--- app/js/tools/clusterer.js
+++ app/js/tools/clusterer.js
@@ -1,64 +1,89 @@
+import whichPolygon from "which-polygon";
+import * as turf from "@turf/turf";
+
 export class Clusterer {
-    #clustersDbscan;
-    #convex;
-    #turfMeta;
-    #turfHelpers;
-
-    /**
-     * @param {import("@turf/clusters-dbscan").clustersDbscan} clustersDbscan
-     * @param {import("@turf/meta")} turfMeta
-     * @param {import("@turf/helpers")} turfHelpers
-     * @param {import("@turf/clone")} clone
-     * @param {import("@turf/convex").convex} convex
-     */
-    constructor(clustersDbscan, turfMeta, turfHelpers, clone, convex) {
-        this.#clustersDbscan = clustersDbscan;
-        this.#turfMeta = turfMeta;
-        this.#turfHelpers = turfHelpers;
-        this.#convex = convex;
-    }
-
     /**
      * @param {import("geojson").FeatureCollection} geojson
-     * @param {{includeClusteredPoints?:boolean}} [options]
      * @param {import("geojson").GeoJsonProperties} [properties={}]
      * @returns {import("geojson").FeatureCollection}
      */
-    addBorders(geojson, options = {}, properties = {}) {
+    addBorders(geojson, properties = {}) {
         /** @type {import("geojson").Feature[]} */
-        const newFeatures = [];
+        const unClusteredPoints = [];
+        /** @type {import("geojson").Feature[]} */
+        const newPolygons = [];
 
-        /** @type {Map<number,import("geojson").Feature[]>} */
+        /** @type {Map<number,import("geojson").Feature<GeoJSON.Point>[]>} */
         const clusters = new Map();
-        this.#turfMeta.featureEach(geojson, (f) => {
-            if (!f.properties) {
+        turf.featureEach(geojson, (point) => {
+            if (!point.properties || point.geometry.type !== "Point") {
                 return;
             }
-            if (typeof f.properties.cluster === "number") {
-                let features = clusters.get(f.properties.cluster);
-                if (!features) {
-                    features = [];
-                    clusters.set(f.properties.cluster, features);
+            if (typeof point.properties.cluster === "number") {
+                let clusterPoints = clusters.get(point.properties.cluster);
+                if (!clusterPoints) {
+                    clusterPoints = [];
+                    clusters.set(point.properties.cluster, clusterPoints);
                 }
-                features.push(f);
-                if (options.includeClusteredPoints) {
-                    newFeatures.push(f);
-                }
+                // @ts-ignore
+                clusterPoints.push(point);
             } else {
-                newFeatures.push(f);
+                unClusteredPoints.push(point);
             }
         });
 
-        for (const features of clusters.values()) {
-            const fc = this.#turfHelpers.featureCollection(features);
-            const border = this.#convex(fc);
-            if (border) {
-                border.properties = { ...properties };
-                newFeatures.push(border);
+        for (const [cluster_num, clusterPoints] of clusters.entries()) {
+            const fc = turf.featureCollection(clusterPoints);
+            const border = turf.concave(fc, { maxEdge: 1 });
+            if (!border) {
+                continue;
             }
+
+            /** @type {import("geojson").Feature[]} */
+            let polygons = [];
+            switch (border.geometry.type) {
+                case "Polygon":
+                    polygons = [border];
+                    break;
+                case "MultiPolygon":
+                    polygons = turf.polygons(
+                        border.geometry.coordinates,
+                    ).features;
+                    break;
+            }
+
+            polygons.forEach((p) => {
+                p.properties = {
+                    cluster: cluster_num,
+                };
+            });
+            newPolygons.push(...polygons);
         }
 
-        return this.#turfHelpers.featureCollection(newFeatures);
+        newPolygons.forEach((p, index) => {
+            p.properties = {
+                ...p.properties,
+                hectares: turf
+                    .convertArea(turf.area(p), "meters", "hectares")
+                    .toFixed(2),
+                pop_num: index,
+                ...properties,
+            };
+        });
+
+        const outsidePoints = this.#findClusteredPointsOutsideOfPolygons(
+            newPolygons,
+            clusters,
+        );
+
+        /** @type {import("geojson").Feature[]} */
+        const features = [
+            ...newPolygons,
+            ...unClusteredPoints,
+            ...outsidePoints,
+        ];
+
+        return turf.featureCollection(features);
     }
 
     /**
@@ -67,7 +92,7 @@ export class Clusterer {
      * @returns {import("geojson").FeatureCollection}
      */
     cluster(geojson, maxDistance = 1) {
-        return this.#clustersDbscan(geojson, maxDistance);
+        return turf.clustersDbscan(geojson, maxDistance);
     }
 
     /**
@@ -76,11 +101,73 @@ export class Clusterer {
      */
     getMaxCluster(geojson) {
         let maxCluster = -1;
-        this.#turfMeta.featureEach(geojson, (f) => {
+        turf.featureEach(geojson, (f) => {
             if (f.properties && f.properties.cluster > maxCluster) {
                 maxCluster = f.properties.cluster;
             }
         });
         return maxCluster === -1 ? undefined : maxCluster;
+    }
+
+    /**
+     * @param {import("geojson").Feature[]} polygons
+     * @param {Map<number,import("geojson").Feature<import("geojson").Point,import("geojson").GeoJsonProperties>[]>} clusters
+     * @returns {import("geojson").Feature<import("geojson").Point>[]}
+     */
+    #findClusteredPointsOutsideOfPolygons(polygons, clusters) {
+        const query = whichPolygon(turf.featureCollection(polygons));
+
+        const initialOutsidePoints = [];
+
+        for (const clusterPoints of clusters.values()) {
+            for (const point of clusterPoints) {
+                const coords = point.geometry.coordinates;
+                const polyProps = query([coords[0], coords[1]]);
+                if (!polyProps) {
+                    initialOutsidePoints.push(point);
+                }
+            }
+        }
+
+        const finalOutsidePoints = [];
+
+        // If any points remain outside, find the distance to the nearest polygon.
+        if (initialOutsidePoints.length) {
+            // Convert all polygons to lines.
+            const polysAsLines = new Map();
+            for (const polygon of polygons) {
+                const cluster = polygon.properties?.cluster;
+                let lines = polysAsLines.get(cluster);
+                if (!lines) {
+                    lines = [];
+                    polysAsLines.set(cluster, lines);
+                }
+                // @ts-ignore
+                lines.push(turf.polygonToLine(polygon));
+            }
+
+            for (const point of initialOutsidePoints) {
+                const cluster = point.properties?.cluster;
+                const lines = polysAsLines.get(cluster);
+                if (!lines) {
+                    console.warn(`cluster ${cluster} has no polygons`);
+                    finalOutsidePoints.push(point);
+                    continue;
+                }
+                let minDistance = Number.MAX_SAFE_INTEGER;
+                for (const line of lines) {
+                    const dist = turf.pointToLineDistance(point, line, {
+                        units: "meters",
+                    });
+                    minDistance = Math.min(minDistance, dist);
+                }
+
+                if (minDistance > 1) {
+                    finalOutsidePoints.push(point);
+                }
+            }
+        }
+
+        return finalOutsidePoints;
     }
 }
--- scripts/int-cluster.js
+++ scripts/int-cluster.js
@@ -1,21 +1,11 @@
 #!/usr/bin/env node
 
-import clustersDbscan from "@turf/clusters-dbscan";
-import convex from "@turf/convex";
-import * as clone from "@turf/clone";
-import * as turfMeta from "@turf/meta";
-import * as turfHelpers from "@turf/helpers";
 import { Command } from "commander";
 import { readFileSync, writeFileSync } from "node:fs";
 import * as path from "node:path";
 import { cwd } from "node:process";
+import * as turf from "@turf/turf";
 import { Clusterer } from "../app/js/tools/clusterer.js";
-
-class NodeClusterer extends Clusterer {
-    constructor() {
-        super(clustersDbscan, turfMeta, turfHelpers, clone, convex);
-    }
-}
 
 const __dirname = cwd();
 
@@ -30,21 +20,37 @@ async function run(program, options) {
     const str = readFileSync(path.join(__dirname, options.input), "utf8");
     /** @type {GeoJSON.FeatureCollection<GeoJSON.Point>} */
     const json = JSON.parse(str);
-    const clusterer = new NodeClusterer();
-    const clustered = clusterer.cluster(json);
-    const bordered = clusterer.addBorders(
-        clustered,
-        {},
-        { fill: "red", "fill-opacity": 0.8 }
-    );
+    const clusterer = new Clusterer();
+    const clustered = clusterer.cluster(json, options.maxdistance);
+    const bordered = clusterer.addBorders(clustered, {
+        fill: "red",
+        "fill-opacity": 0.8,
+    });
+
+    // Go through remaining points and color code them.
+    turf.featureEach(bordered, (point) => {
+        if (point.geometry.type === "Point") {
+            const properties = point.properties ?? {};
+            if (properties.dbscan === "noise") {
+                properties["marker-color"] = "yellow";
+            }
+        }
+    });
+
     writeFileSync(
-        path.join(__dirname, "tmp/clustered.geojson"),
-        JSON.stringify(bordered)
+        path.join(__dirname, options.output),
+        JSON.stringify(bordered),
     );
 }
 
 const program = new Command();
 program.option("-i, --input <path>", "The path to the input file.");
+program.option("-o, --output <path>", "The path to the output file.");
+program.option(
+    "-d, --maxdistance <km>",
+    "The maximum distance to use in the clustering algorithm, in kilometers.",
+    "1",
+);
 program.action((options) => run(program, options));
 
 await program.parseAsync();
