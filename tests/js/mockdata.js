607c8a5d1947cac4f6e4224075ea27f7814e33dc
John Kenny <johnkenny54@yahoo.com>
2025-03-20 09:13:22 -0700

    chore: update unit tests

--- tests/js/mockdata.js
+++ tests/js/mockdata.js
@@ -1,12 +1,15 @@
 /**
  * @param {import("../../app/js/types.js").INatDataTaxon} taxon
+ * @param {{taxon_geoprivacy?:string,geoprivacy?:string|null,private_location?:string}} [options={}]
  * @returns {import("../../app/js/types.js").INatDataObs}
  */
-export function makeObservationData(taxon) {
+export function makeObservationData(taxon, options = {}) {
     const rawObservation = {
-        taxon_geoprivacy: "",
-        geoprivacy: "",
-        private_location: "",
+        taxon_geoprivacy: options.taxon_geoprivacy ?? "",
+        geoprivacy: options.geoprivacy ?? "",
+        private_location: options.private_location ?? "",
+        positional_accuracy: 0,
+        public_positional_accuracy: 0,
         id: "",
         location: "",
         observed_on_details: { date: "" },
